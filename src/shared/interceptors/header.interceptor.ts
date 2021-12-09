95263946843e3878adbd6cb0bdc42ecad145c513
annguyen <annguyen.it.utc@gmail.com>
2021-12-09 15:21:58 +0700

    Apply refresh token

--- src/shared/interceptors/header.interceptor.ts
+++ src/shared/interceptors/header.interceptor.ts
@@ -4,14 +4,22 @@ import {
   HttpInterceptor,
   HttpHandler,
   HttpRequest,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable, of, throwError } from 'rxjs';
 import { TokenService } from '@services/core/token.service';
+import { catchError, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { UserInfoService } from '@services/core/user-info.service';
 
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
   /** CONSTRUCTOR */
-  constructor(private tokenService: TokenService) {}
+  constructor(
+    private router: Router,
+    private tokenService: TokenService,
+    private userInfoService: UserInfoService
+  ) {}
 
   /** IMPLEMENTATIONS */
   public intercept(
@@ -26,6 +34,27 @@ export class HeaderInterceptor implements HttpInterceptor {
 
     const authReq = req.clone({ headers });
 
-    return next.handle(authReq);
+    return next.handle(authReq).pipe(
+      tap(
+        () => {},
+        (error) => {
+          if (!(error instanceof HttpErrorResponse)) return;
+
+          switch (error.status) {
+            case 401:
+              this.tokenService.clear();
+              this.userInfoService.clear();
+              void this.router.navigate(['/login']);
+              break;
+            case 500: {
+              const token = error.headers.get('Authorization');
+              if (token) {
+                this.tokenService.set(token);
+              }
+            }
+          }
+        }
+      )
+    );
   }
 }
