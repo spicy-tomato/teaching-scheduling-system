d3c730fc311981b03eb6f37fe2a8a0dc49eba8dc
annguyen <annguyen.it.utc@gmail.com>
2021-12-31 10:33:30 +0700

    Update time of shift

--- src/shared/constants/core/core.constant.ts
+++ src/shared/constants/core/core.constant.ts
@@ -9,4 +9,32 @@ export class CoreConstant {
     2: 'Bài tập',
     3: 'Thực hành',
   };
+  public static readonly SHIFTS: {
+    [key: string]: { start: number[]; end: number[] };
+  } = {
+    '1': {
+      start: [7, 0],
+      end: [9, 25],
+    },
+    '2': {
+      start: [9, 35],
+      end: [12, 0],
+    },
+    '3': {
+      start: [13, 0],
+      end: [15, 25],
+    },
+    '4': {
+      start: [15, 35],
+      end: [18, 0],
+    },
+    '5-1': {
+      start: [18, 5],
+      end: [20, 30],
+    },
+    '5-2': {
+      start: [18, 5],
+      end: [21, 20],
+    },
+  };
 }
