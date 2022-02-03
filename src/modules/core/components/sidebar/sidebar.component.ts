9aa5fa7df079d1ba73d92965dd231dc969e1e577
annguyen <annguyen.it.utc@gmail.com>
2022-02-03 12:45:49 +0700

    Loading sidebar

--- src/modules/core/components/sidebar/sidebar.component.ts
+++ src/modules/core/components/sidebar/sidebar.component.ts
@@ -1,14 +1,25 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SidebarConstant } from '@shared/constants';
-import { SidebarItem } from 'src/shared/models';
+import * as fromAppShell from '@modules/core/components/app-shell/state';
+import { Store } from '@ngrx/store';
+import { Nullable, Teacher } from '@shared/models';
+import { Observable } from 'rxjs';
+import { fadeIn, fadeOut } from '@shared/animations';
 
 @Component({
   selector: 'tss-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [fadeIn, fadeOut],
 })
 export class SidebarComponent {
   /** PUBLIC PROPERTIES */
-  public readonly items: SidebarItem[] = SidebarConstant.items;
+  public user$: Observable<Nullable<Teacher>>;
+  public readonly items = SidebarConstant.items;
+
+  /** CONSTRUCTOR */
+  constructor(appShellStore: Store<fromAppShell.AppShellState>) {
+    this.user$ = appShellStore.select(fromAppShell.selectTeacher);
+  }
 }
