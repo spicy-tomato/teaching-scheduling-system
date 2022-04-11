70f921f43a16012431ad8a64b4edfac4a013d344
annguyen <annguyen.it.utc@gmail.com>
2022-04-11 11:49:33 +0700

    feat!: change API

--- src/services/common-info.service.ts
+++ src/services/common-info.service.ts
@@ -2,13 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
-import { FacultyDta } from '@shared/dtas';
-import {
-  Faculty,
-  AcademicYear,
-  SimpleMapModel,
-  SimpleModel,
-} from 'src/shared/models';
+import { AcademicYear, ResponseModel, SimpleMapModel, SimpleModel } from 'src/shared/models';
 import { AppSettingsService } from './core/app-settings.service';
 import { BaseDataService } from './core/base-data.service';
 import { LocalDataService } from './core/local-data.service';
@@ -23,12 +17,6 @@ export class CommonInfoService extends BaseDataService {
     private readonly localDataService: LocalDataService
   ) {
     super();
-  }
-
-  public getFaculty(): Observable<Faculty[]> {
-    return this.http
-      .get<FacultyDta[]>(this.url + 'faculty')
-      .pipe(map((result) => result.map((x) => Faculty.parse(x))));
   }
 
   public getAcademicYear(): Observable<AcademicYear> {
@@ -56,10 +44,12 @@ export class CommonInfoService extends BaseDataService {
     );
   }
 
-  public getDepartments(): Observable<SimpleMapModel<string, SimpleModel[]>[]> {
-    return this.http.get<SimpleMapModel<string, SimpleModel[]>[]>(
-      this.url + 'faculties'
-    );
+  public getFaculties(): Observable<
+    ResponseModel<SimpleMapModel<string, SimpleModel[]>[]>
+  > {
+    return this.http.get<
+      ResponseModel<SimpleMapModel<string, SimpleModel[]>[]>
+    >(this.url + 'faculties');
   }
 
   public getRooms(): Observable<string[]> {
