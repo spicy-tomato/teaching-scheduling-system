3a83c5b4b7f6eefb41b10e6db02f97a02f59c627
annguyen <annguyen.it.utc@gmail.com>
2022-07-09 00:16:44 +0700

    fix(calendar): fix responsive bug for small laptop screen

---
+++ libs/core/pipes/shorten-name/README.md
@@ -0,0 +1,7 @@
+# core-pipes-shorten-name
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test core-pipes-shorten-name` to execute the unit tests.
--- libs/web/shared/data-access/services/src/lib/core/token.service.ts
+++ libs/web/shared/data-access/services/src/lib/core/token.service.ts
@@ -8,16 +8,10 @@ export class TokenService {
   public static readonly DATE_PIPE_TOKEN = new InjectionToken<DatePipe>(
     'datePipe'
   );
-  public static readonly SHORTEN_NAME_PIPE_TOKEN = new InjectionToken<DatePipe>(
-    'shortenNamePipe'
-  );
 
   public getToken<T>(name: string): InjectionToken<T> {
     if (name === 'datePipe') {
       return TokenService.DATE_PIPE_TOKEN;
-    }
-    if (name === 'shortenNamePipe') {
-      return TokenService.SHORTEN_NAME_PIPE_TOKEN;
     }
 
     throw `No token with name ${name} found`;
