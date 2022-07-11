6fdbbb9e8a871c0d6d80de80220e39f1af07c972
annguyen <annguyen.it.utc@gmail.com>
2022-07-12 00:49:59 +0700

    feat(statistic): responsive

--- libs/web/statistic/ui/change-schedule-filter/src/lib/change-schedule-filter.module.ts
+++ libs/web/statistic/ui/change-schedule-filter/src/lib/change-schedule-filter.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChangeScheduleFilterComponent } from './change-schedule-filter.component';
-import { TuiInputDateRangeModule } from '@taiga-ui/kit';
-import { ReactiveComponentModule } from '@ngrx/component';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveComponentModule } from '@ngrx/component';
 import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiInputDateRangeModule } from '@taiga-ui/kit';
+import { ChangeScheduleExportDirectiveModule } from '@teaching-scheduling-system/web/statistic/ui/change-schedule-export-directive';
+import { ChangeScheduleFilterComponent } from './change-schedule-filter.component';
 
 const NGRX = [ReactiveComponentModule];
 const TAIGA_UI = [TuiInputDateRangeModule, TuiButtonModule];
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, ...NGRX, ...TAIGA_UI],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ChangeScheduleExportDirectiveModule,
+    ...NGRX,
+    ...TAIGA_UI,
+  ],
   declarations: [ChangeScheduleFilterComponent],
   exports: [ChangeScheduleFilterComponent],
 })
