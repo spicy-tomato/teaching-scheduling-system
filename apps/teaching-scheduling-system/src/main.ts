eff0967e52e1ef2554a7c5e42e1a55068c3fd018
annguyen <annguyen.it.utc@gmail.com>
2022-07-12 14:02:33 +0700

    refactor: format code

--- apps/teaching-scheduling-system/src/main.ts
+++ apps/teaching-scheduling-system/src/main.ts
@@ -7,9 +7,7 @@ import { environment } from './environments/environment';
 
 import Echo from 'laravel-echo';
 
-
 (window as any).Pusher = require('pusher-js');
-
 
 (window as any).Echo = new Echo({
   broadcaster: 'pusher',
@@ -20,24 +18,20 @@ import Echo from 'laravel-echo';
   authEndpoint: 'http://localhost/education-supporting-system/public/api/auth',
   // authEndpoint: 'http://localhost/education-supporting-system/public/broadcasting/auth',
   // authEndpoint: 'http://localhost/education-supporting-system/public/channel/my-channel',
-
-
 });
 
 console.log(window);
 
-(window as any).Echo.channel(`public-channel`)
-  .listen('.public-event', (e: any) => {
-    alert(e)
-  });
+(window as any).Echo.channel(`public-channel`).listen(
+  '.public-event',
+  (e: any) => {
+    alert(e);
+  }
+);
 
-
-(window as any).Echo.private(`my-channel`)
-  .listen('.my-event', (e: any) => {
-    alert(e)
-  });
-
-
+(window as any).Echo.private(`my-channel`).listen('.my-event', (e: any) => {
+  alert(e);
+});
 
 fetch('/assets/settings/app-settings.json')
   .then((res) => res.json())
--- pusher.d.ts
+++ pusher.d.ts
@@ -1,16 +1,13 @@
 declare global {
-    interface Window {
-        Echo: any; 
-        Pusher: any; 
-
-    }
-
+  interface Window {
+    Echo: any;
+    Pusher: any;
   }
+}
 
-  interface CustomWindow extends Window {
-    Echo? : any;
-    Pusher?: any;
-  }
+interface CustomWindow extends Window {
+  Echo?: any;
+  Pusher?: any;
+}
 
-  export const customWindow: CustomWindow;
-
+export const customWindow: CustomWindow;
