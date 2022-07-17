ae6dc10afa23080916321dcef5d9abf3112ca121
annguyen <annguyen.it.utc@gmail.com>
2022-07-17 17:08:01 +0700

    ci: setup test with jest

--- libs/core/directives/recreate-view-key/src/lib/recreate-view-key.directive.spec.ts
+++ libs/core/directives/recreate-view-key/src/lib/recreate-view-key.directive.spec.ts
@@ -1,8 +1,40 @@
+import { Component, DebugElement, DebugNode } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RecreateViewKeyDirective } from './recreate-view-key.directive';
 
+@Component({
+  template: `
+    <div>Without Directive</div>
+    <div *tssRecreateViewKey="null">Default</div>
+  `,
+})
+class TestComponent {}
+
 describe('RecreateViewKeyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RecreateViewKeyDirective();
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<TestComponent>;
+  let elementsWithDirective: DebugNode[];
+  let bareElement: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RecreateViewKeyDirective, TestComponent],
+    });
+    fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    elementsWithDirective = fixture.debugElement.queryAllNodes(
+      By.directive(RecreateViewKeyDirective)
+    );
+    bareElement = fixture.debugElement.query(
+      By.css(':not([tssRecreateViewKey])')
+    );
+  });
+
+  it('should have bare element', () => {
+    expect(bareElement).toBeTruthy();
+  });
+
+  it('should have 1 element(s) with directive', () => {
+    expect(elementsWithDirective.length).toBe(1);
   });
 });
