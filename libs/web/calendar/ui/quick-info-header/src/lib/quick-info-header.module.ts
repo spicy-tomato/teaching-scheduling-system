705bdcc949560c2f61fe331edf1de26a108d0055
annguyen <annguyen.it.utc@gmail.com>
2022-07-20 19:10:48 +0700

    fix(calendar): use tuiButton in quick view dialog instead of default button

--- libs/web/calendar/ui/quick-info-header/src/lib/quick-info-header.module.ts
+++ libs/web/calendar/ui/quick-info-header/src/lib/quick-info-header.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuickInfoHeaderComponent } from './quick-info-header.component';
+import { TuiButtonModule } from '@taiga-ui/core';
+
+const TAIGA_UI = [TuiButtonModule];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ...TAIGA_UI],
   declarations: [QuickInfoHeaderComponent],
   exports: [QuickInfoHeaderComponent],
 })
