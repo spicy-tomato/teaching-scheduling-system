cffcdd4f5d30b3dec5eab1a3cd50e7c18788b758
annguyen <annguyen.it.utc@gmail.com>
2022-08-15 00:19:04 +0700

    fix(exam/assign): cannot re-open assign dialog

--- libs/web/exam/ui/assign-teacher-dialog/src/lib/assign-teacher-dialog.module.ts
+++ libs/web/exam/ui/assign-teacher-dialog/src/lib/assign-teacher-dialog.module.ts
@@ -9,7 +9,6 @@ import {
 } from '@taiga-ui/core';
 import { TuiDataListWrapperModule, TuiMultiSelectModule } from '@taiga-ui/kit';
 import { AssignTeacherDialogComponent } from './assign-teacher-dialog.component';
-import { AssignTeacherDialogStore } from './store';
 
 const NGRX = [ReactiveComponentModule];
 const TAIGA_UI = [
@@ -24,6 +23,5 @@ const TAIGA_UI = [
   imports: [CommonModule, ReactiveFormsModule, ...NGRX, ...TAIGA_UI],
   declarations: [AssignTeacherDialogComponent],
   exports: [AssignTeacherDialogComponent],
-  providers: [AssignTeacherDialogStore],
 })
 export class AssignTeacherDialogModule {}
